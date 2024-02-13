7e7b274988f0325dafc8acc0f6ba4434250504b4
Philippe Hässig <philippe@bespinian.io>
2024-02-13 13:49:34 +0100

    Deploy gems in their own layer

--- infra/ruby-func/main.tf
+++ infra/ruby-func/main.tf
@@ -5,11 +5,34 @@ data "archive_file" "deploy-package" {
   source_dir  = var.source-dir
 }
 
+data "archive_file" "gem-layer" {
+  output_path = "${var.dist-path}/${var.function-name}-gems.zip"
+  type        = "zip"
+  source_dir  = "${var.source-dir}-gems"
+}
+
 resource "aws_s3_object" "deploy-package" {
   bucket = var.bucket-name
   key    = "${var.function-name}-source.zip"
   source = data.archive_file.deploy-package.output_path
   etag   = data.archive_file.deploy-package.output_md5
+}
+
+resource "aws_s3_object" "gems-layer" {
+  bucket = var.bucket-name
+  key    = "${var.function-name}-gems.zip"
+  source = data.archive_file.gem-layer.output_path
+  etag   = data.archive_file.gem-layer.output_md5
+}
+
+resource "aws_lambda_layer_version" "gems-layer" {
+  s3_bucket  = var.bucket-name
+  s3_key     = aws_s3_object.gems-layer.key
+  layer_name = "${var.function-name}-gems"
+
+  compatible_runtimes = [var.runtime]
+
+  source_code_hash = data.archive_file.gem-layer.output_base64sha256
 }
 
 resource "aws_lambda_function" "main" {
@@ -25,4 +48,6 @@ resource "aws_lambda_function" "main" {
   source_code_hash = data.archive_file.deploy-package.output_base64sha256
 
   depends_on = [aws_s3_object.deploy-package]
+
+  layers = [aws_lambda_layer_version.gems-layer.arn]
 }
